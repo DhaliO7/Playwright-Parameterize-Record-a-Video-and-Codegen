d745c32c0826967e39e8834f890e56d671d5ffa0
Dhali <carlosdhali@hotmail.com>
2025-05-18 15:06:21 -0600

    Primer commit con la actividad 3

---
+++ tests_dhali/browserContext.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect, firefox } from "@playwright/test";
+
+test("Exercise 4 - Browsers & Browser context", async () => {
+  
+  const browser = await firefox.launch();
+
+  
+  console.log("Browser context length (inicial):", browser.contexts().length);
+
+  
+  const context = await browser.newContext();
+
+  
+  console.log("Browser context length (después de crear uno):", browser.contexts().length);
+
+  
+  const page = await context.newPage();
+
+  
+  await page.goto("https://playwright.dev");
+
+  
+  await context.close();
+  await browser.close();
+});
